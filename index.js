cef4da47e541a8c81ff935cff1d37885071ca774
Casey Ann Cumbow <cumbow8@gmail.com>
2017-10-03 15:13:32 -0600

    Minor updates

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-var addNewButton = $('.add-new');
-var buttonDiv = $('.parent');
-var allButtons = $('.parent .button')
+var $addNewButton = $('.add-new');
+var $postDiv = $('.post');
+var $newForm = $('.new-form')
 
 function showAlert() {
   alert('You clicked me!');
 }
 
-addNewButton.on('click', function () {
-  var newButton = buttonDiv.append("<button class='button'>New click me button!</button>")
+var form = "<form class='new-form'>Title: <input type='text' name='title'><br>Content: <input type='text' name='content'><br><input type='submit'></form>"
+
+$addNewButton.on('click', function(event) {
+  $postDiv.append(form)
 });
 
-allButtons.on('click', showAlert)
+$newForm.on('submit', function(event){
+  event.preventDefault()
+  var title = $(this).find("input[name='title']").val()
+  var content = $(this).find("input[name='title']").val()
+  $postDiv.append(postHTML(title, content))
+  $('form').hide()
+  hideForm()
+})
+
+function hideForm() {
+  $('form').hide()
+}
+
+function postHTML(title, content) {
+  return "<h1>" + title + "</h1><h3>" + content + "</h3>"
+}
